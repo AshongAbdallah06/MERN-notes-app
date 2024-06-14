10863fb4a0120c372f5405bf1b04b0d46d961f73
AshongAbdallah06 <ashongabdalla15@gmail.com>
2024-06-14 18:20:46 +0000

    --amend
    add credentials: include to signup request

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -12,6 +12,7 @@ const Signup = () => {
 			method: "POST",
 			body: JSON.stringify({ username, email, password }),
 			headers: { "Content-Type": "application/json" },
+			credentials: "include",
 		});
 
 		const data = await response.json();
